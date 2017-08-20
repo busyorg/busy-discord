67f06272d08602939b54aceea7652e7306248ad1
adcpm <fabien@bonustrack.co>
2017-08-20 22:57:45 +0700

    Use websocket instead of JSON RPC

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const https = require('https');
 
 http.globalAgent.maxSockets = 100;
 https.globalAgent.maxSockets = 100;
-steem.api.setOptions({ transport: 'http' });
+//steem.api.setOptions({ transport: 'http' });
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 const client = redis.createClient(process.env.REDIS_URL);
