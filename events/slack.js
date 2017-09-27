eadf5396b27f2695498ff63c1db7184758c5fbcf
adcpm <fabien@bonustrack.co>
2017-09-27 21:57:03 +0700

    Fix slackbot edit/new

--- events/slack.js
+++ events/slack.js
@@ -43,7 +43,7 @@ const postMessage = (channel, op) => {
   } catch (err) { }
 
   let message = `*<https://nd.busy.org/${op[1].parent_permlink}/@${op[1].author}/${op[1].permlink}|@${op[1].author}>* ${jsonMetadata.app} `;
-  message += op[1].body.includes('@@') ? 'New ' : 'Edit ';
+  message += op[1].body.includes('@@') ? 'edit ' : 'new ';
   message += op[1].parent_author ? 'comment' : 'post';
   bot.postMessageToChannel(
     channel,
@@ -51,7 +51,7 @@ const postMessage = (channel, op) => {
     { mrkdwn_in: ["text"] }
   );
 
-  let log = op[1].body.includes('@@') ? 'New ' : 'Edit ';
+  let log = op[1].body.includes('@@') ? 'Edit ' : 'New ';
   log += op[1].parent_author ? 'comment ' : 'post ';
   log += `@${op[1].author}/${op[1].permlink} with ${jsonMetadata.app}: `;
   if (jsonMetadata.app.includes('busy/')) {
