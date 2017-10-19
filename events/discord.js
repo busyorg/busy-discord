4c896b7f13b014eca78527d3ed0d73d8567bcf72
adcpm <fabien@bonustrack.co>
2017-10-20 01:57:04 +0700

    Add discord bot, remove slack bot

--- events/discord.js
+++ events/discord.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const channelId = '370640103976075274';
+const channelId = process.env.DISCORD_CHANNEL_ID;
 let started;
 
 bot.on('ready', () => {
