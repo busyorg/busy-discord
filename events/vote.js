7e17775c249d5e92779c431a62b76176027148df
Fabien <bonustrack@users.noreply.github.com>
2017-12-18 13:44:32 +0700

    Require 150 whale to get max upvote

--- events/vote.js
+++ events/vote.js
@@ -12,7 +12,7 @@ const postingWif = process.env.STEEM_POSTING_WIF;
 const delay = parseInt(process.env.STEEM_VOTE_DELAY || 43200);
 
 const MIN_VESTS = 10000000; // Dolphin
-const MAX_VESTS = 100000000000; // 100 Whales
+const MAX_VESTS = 150000000000; // 150 Whales
 
 const calculateVotingPower = async (username) => {
   const url = `https://steemdb.com/api/accounts?account[]=${username}`;
